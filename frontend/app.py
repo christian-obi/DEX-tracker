ca3f565117749ca7529ab55bdab398cce7f74d27
vhictoirya <140712173+vhictoirya@users.noreply.github.com>
2025-08-20 16:18:03 +0100

    updated app,py

---
+++ frontend/app.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import joblib
+import plotly.express as px
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.set_page_config(page_title="DEX Tracker", layout="wide")
+
